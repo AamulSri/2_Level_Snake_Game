bfacf7bb8acd39f0210c01a12c187251545d86d5
AamulSri <aamul24.vns@gmail.com>
2024-05-14 14:40:38 +0530

    Start Of Code

---
+++ SnakeGame.java
@@ -0,0 +1,12 @@
+package SnakeGameSetUp;
+
+public class SnakeGame {
+ 
+ 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         new GameFrame("NEW_PLAYER");
+         
+	}
+
+}
